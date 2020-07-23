d2a340b9102b2443f1776608f349ab889165a2e9
KathirvelChandrasekaran <kathirvel98@outlook.com>
2020-07-23 11:35:44 +0530

    added scream post in Home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
+import Grid from "@material-ui/core/Grid";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+import Screams from "../components/screams";
 export class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home</h1>
-            </div>
-        )
-    }
+  state = {
+    screams: null,
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then((res) => {
+        this.setState({
+          screams: res.data,
+        });
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => (
+        <Screams key={scream.screamId} scream={scream}></Screams>
+      ))
+    ) : (
+      <CircularProgress color="secondary" />
+    );
+    return (
+      <div>
+        <Grid container spacing={10}>
+          <Grid item sm={8} xs={12}>
+            {recentScreamsMarkup}
+          </Grid>
+          <Grid item sm={4} xs={12}>
+            <p>Profile</p>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
